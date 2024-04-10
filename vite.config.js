78ed79731606ac0d4986acb64cdfe1a7f7dac57e
Rishav Sah <rishavsah4u@gmail.com>
2024-04-10 10:22:57 +0545

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/catalix-dashboard-ui",
+  base: "/catalix-ui",
   plugins: [react()],
 })
